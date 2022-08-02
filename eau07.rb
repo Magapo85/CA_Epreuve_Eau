8d43a3f5555cc532112fa54862b3c1d9547904d6
Magapo85 <petithomme.guillaume@gmail.com>
2022-08-02 23:06:41 +0200

    exo 7

--- eau07.rb
+++ eau07.rb
@@ -21,13 +21,13 @@ def longueurArgument(mot)
 end
 
 #fonction pour trouver un caractere dans un array, comme .index()
-def trouverDansArray(carac, array)
-        sortie = "ok"
-        index = longueurArgument(array)
+def trouverDansArray(carac, array1, array2)
+        sortie = 0
+        index = longueurArgument(array1)
         i = 0
-        while i < longueurArgument(array) && sortie == "ok"
-                if array[i] == carac
-                        sortie = "erreur"
+        while i < longueurArgument(array1) && sortie == 0
+                if array1[i] == carac || array2[i] == carac
+                        sortie = 1
                         index = i
                 end
                 i = i + 1
@@ -35,14 +35,38 @@ def trouverDansArray(carac, array)
         return index, sortie
 end
 
-def trouverLettre()
+def trouverLettre(carac, etat)
 	maj = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-	min = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']  
-		
+	min = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] 
+	i, pastrouve = trouverDansArray(carac, maj, min) 
+	if pastrouve == 0 
+		new = carac
+	else
+		if etat == 0
+			new = maj[i] 
+			etat = 1
+		else
+		        new = min[i]
+                        etat = 0
+		end
+	end
+	return new, etat
 end
 
-if nombreArgument != 1 || (ARGV[0] != "0" && ARGV[0].to_i <= 0) || fauxEntier(ARGV[0]) == "erreur"
+def atlMajMin(mot)
+	i = 0
+	etat = 0 #0=maj;1=min
+	sortie = ""
+	while mot[i]
+		new, etat = trouverLettre(mot[i], etat)
+		sortie = sortie + new
+		i = i + 1
+	end
+	return sortie
+end
+
+if nombreArgument != 1 || (ARGV[0] != "0" && ARGV[0].to_i != 0)
         puts "error"
 else
-
+	puts atlMajMin(ARGV[0])
 end
