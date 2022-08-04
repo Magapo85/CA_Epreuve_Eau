945d214e0e90e6b4c9f26a0061c14848eaaf7769
Magapo85 <petithomme.guillaume@gmail.com>
2022-08-04 23:32:03 +0200

    exo 8 suite

--- eau08.rb
+++ eau08.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-#programme qui met en majuscule une lettre sur deux d'une chaine de caracteres. Seule les lettres (A-Z, a-z) sont prises en compte Afficher error
+#programme qui met en majuscule la premiere lettre de chaque mot d'une chaine de caracteres. Les autres lettres devront etre en minuscules. Les mots ne sont delimites que par un espace, une tabulation ou un retour a la ligne Afficher error
 
 #fonction utile qui trouve le nombre d'argument comme .length()
 def nombreArgument
@@ -21,44 +21,56 @@ def longueurArgument(mot)
 end
 
 #fonction pour trouver un caractere dans un array, comme .index()
-def trouverDansArray(carac, array1, array2)
+def trouverDansArray(carac, array1, array2, sepMot)
         sortie = 0
         index = longueurArgument(array1)
-        i = 0
-        while i < longueurArgument(array1) && sortie == 0
-                if array1[i] == carac || array2[i] == carac
-                        sortie = 1
-                        index = i
-                end
-                i = i + 1
+	if longueurArgument(carac) == 1
+		i = 0
+		while i < longueurArgument(array1) && sortie == 0
+                	if array1[i] == carac || array2[i] == carac
+                        	sortie = 1
+                        	index = i
+                	end
+                	i = i + 1
+		end
+	else	
+		i = 0
+        	while i < longueurArgument(sepMot) && sortie == 0
+			if carac[0] == sepMot[i]
+				b = 0 
+				while b < longueurArgument(array1) && sortie == 0
+					if array1[b] == carac[1] || array2[b] == carac[1]
+                        			sortie = 1
+                        			index = b
+					end
+					b = b + 1
+				end
+                	end
+                	i = i + 1
+		end
         end
         return index, sortie
 end
 
-def trouverLettre(carac, etat)
+def trouverLettre(carac)
         maj = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
         min = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        i, pastrouve = trouverDansArray(carac, maj, min)
+	sepMot = [' ','		','\n'] #['\r','\s','\b','\n']
+        i, pastrouve = trouverDansArray(carac, maj, min, sepMot)
         if pastrouve == 0
-                new = carac
+                new = carac[0]
         else
-                if etat == 0
-                        new = maj[i]
-                        etat = 1
-                else
-                        new = min[i]
-                        etat = 0
-                end
+                new = maj[i]
         end
-        return new, etat
+        return new
 end
 
-def atlMajMin(mot)
-        i = 0
-        etat = 0 #0=maj;1=min
+def putMaj(mot)
+        i = 1
         sortie = ""
+	sortie = sortie + trouverLettre(mot[0])
         while mot[i]
-                new, etat = trouverLettre(mot[i], etat)
+                new = trouverLettre(mot[i-1]+mot[i])
                 sortie = sortie + new
                 i = i + 1
         end
@@ -66,3 +78,7 @@ def atlMajMin(mot)
 end
 
 if nombreArgument != 1 || (ARGV[0] != "0" && ARGV[0].to_i != 0)
+	puts "error"
+else
+	puts putMaj(ARGV[0])
+end
