14fe291db5843defd6753c7be52a263539a70db7
Magapo85 <petithomme.guillaume@gmail.com>
2022-08-07 21:37:45 +0200

    exo 12

--- eau12.rb
+++ eau12.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 #programme qui affiche la difference minimum absolue entre deux elements d'un tableau constitue uniquement de nombres. Nombres negatifs acceptes afficher error sinon
+require 'bigdecimal'
+require 'bigdecimal/util' #to_d
 
 #fonction utile qui trouve le nombre d'argument comme .length()
 def nombreArgument
@@ -35,24 +37,57 @@ def trouverDansArray(carac, array)
 end
 
 #fonction utile qui test si l'argument (nombre suppose entier) contient . ou , avant conversion et calcul 
-def fauxEntier(nombre)
-        if trouverDansArray(",", nombre)[1] == "erreur" || trouverDansArray(".", nombre)[1] == "erreur"
-                return "erreur"
-	else	
-		return "ok"
+def fauxFloatEntier()
+	i = 0
+	sortie = "ok"
+	while ARGV[i] && sortie == "ok"
+        	if trouverDansArray(",", ARGV[i])[1] == "erreur" || (ARGV[i] != (ARGV[i].to_i).to_s && ARGV[i] != (ARGV[i].to_f).to_s)
+                	sortie = "erreur"
+		end
+		i = i + 1
 	end
+	return sortie
 end
 
 #coeur du programme
-if nombreArgument < 2 || ARGV[0] != (ARGV[0].to_i).to_s #|| fauxEntier(ARGV[0]) == "erreur" || fauxEntier(ARGV[1]) == "erreur"
+if nombreArgument <= 1 || fauxFloatEntier() == "erreur"
+#commentaire bloc
+=begin
+	i = 0
+	while ARGV[i]
+		puts ARGV[i]
+		puts ARGV[i].to_i
+		puts ARGV[i].to_f
+		i = i + 1
+	end
+=end
         puts "error"
 else
-	sortie = 
-	sortie = ARGV[0]
-	debut = ARGV[0].to_i + 1
-	while debut < ARGV[1].to_i
-		sortie = sortie + " " + debut.to_s
-		debut = debut + 1 
+	#puts BigDecimal(BigDecimal(ARGV[0]) - BigDecimal(ARGV[1]))
+	#puts (ARGV[0].to_d - ARGV[1].to_d).to_d
+=begin
+	puts ARGV[0]
+        puts ARGV[1]
+	puts ARGV[0].to_s
+        puts ARGV[1].to_s
+	puts ARGV[0].to_i
+	puts ARGV[1].to_i
+	puts ARGV[0].to_f
+        puts ARGV[1].to_f
+	puts ARGV[0].to_d
+        puts ARGV[1].to_d
+=end
+	sortie = (((ARGV[0].to_d - ARGV[1].to_d).to_d ** 2).to_d ** 0.5).to_d
+	a = 0
+	while a <= nombreArgument() - 2
+		b = a + 1
+		while ARGV[b]
+			if (((ARGV[a].to_d - ARGV[b].to_d).to_d ** 2).to_d ** 0.5).to_d < sortie
+				sortie = (((ARGV[a].to_d - ARGV[b].to_d).to_d ** 2).to_d ** 0.5).to_d
+			end
+			b = b + 1
+		end
+		a = a + 1
 	end
-        puts sortie
+        puts sortie.to_f
 end
